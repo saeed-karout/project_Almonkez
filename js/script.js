230e3817645b97c6f5fb0377d5bf370597e5799a
saeed-karout <saed.karot1@gmail.com>
2023-06-26 19:32:48 +0300

    add 5 Event

--- js/script.js
+++ js/script.js
@@ -1,11 +1,55 @@
 
+/*function myFunction() {
+    alert("يتم تحميل الصفحة يرجى الانتظار قليلا ");
+  }
 
+*/
 
-
-
-function change_text_btn(){
-    document.getElementById("open_windwo").innerHTML="انتقل الى ماذا نقدم في الشريط العلوي "
+function loadImage() {
+  alert("يتم تحميل الصور");
 }
+
+function change_text_btn1()
+{
+  document.getElementById("btn_ord").innerHTML="اطلبني الان ";
+}
+function text_btn1()
+{
+    document.getElementById("btn_ord").innerHTML="انتقل الى للتواصل في الشريط العلوي ";
+}
+
+//تغيير لون العنصر المحدد في صفحة المتدربون 
+function change_color(elmnt, clr) {
+  elmnt.style.color = clr;
+}
+
+
+
+function change_text_btn2()
+{
+  document.getElementById("btn_start").innerHTML="ابدأ مشروعك";
+}
+function text_btn2()
+{
+  document.getElementById('btn_start').innerHTML="انتقل الى ماذا نقدم في الشريط العلوي ";
+}
+
+
+
+// تغيير حجم الصورة عندما يقف مؤشر الماوس فوقها 
+function bigImg(x) {
+  x.style.height = "350px";
+  
+}
+
+function normalImg(x) {
+  x.style.height = "220px";
+ 
+}
+
+
+    
+
 
 
 
